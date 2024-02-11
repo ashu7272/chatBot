cd299a869598a527d500f6fe433012a4cdd03082
ashutosh <ashutoshjha7272@gmail.com>
2024-02-11 13:11:40 +0530

    staticChatbot

--- practical_chattbot/chat.js
+++ practical_chattbot/chat.js
@@ -1,33 +1,93 @@
 
-const sendEl = document.querySelector(".send i");
-const inputEl = document.querySelector("#txtinput");
-const chatbodyEl = document.querySelector(".chat_body");
+const sendEl = document.querySelector(".send");
+const inputEl = document.querySelector("#chat_body");
+
+const txtinputEl = document.querySelector("#txtinput");
+const loadingEl = document.querySelector(".loading");
+
+const responseobj = {
+   hello : "How are you",
+   hey : "What are you doing?",
+   today: new Date().toLocaleDateString(),
+   time: new Date().toLocaleTimeString()
+
+};
+const toggleLoading = (show)=>{
+   loadingEl.classList.toggle("hide",show);
+}
+
+const savedScrollbarPosition = ()=>{
+
+   if(inputEl.scrollTop>0){
+     inputEl.savedScrillbarPosition = inputEl.scrollHeight
+   }
+}
 
 
-
-inputEl.addEventListener("keyup",(elm)=>{
-  if(elm.keyCode === 13){
-   renderMessage();
-  }
+txtinputEl.addEventListener("keyup",(elm)=>{
+   if(elm.keyCode === 13){
+      renderMessage()
+   }
 })
 
 
-const userMessage = (txt)=>{
-  const message = document.createElement("div");
+function getChat (inputMessage,type){
+    
+   let className = "user_message";
+   if(type !== "user"){
+      className = "chatbot_message";
+   }
 
-   message.classList.add("showmessage");
-   message.textContent = txt;
-  chatbodyEl.append(message.textContent);
+   const messageCover = document.createElement("div");
+   messageCover.classList.add(className);
+   messageCover.textContent = inputMessage;
 
- }
+   setTimeout(()=>{
+      inputEl.appendChild(messageCover)
+      savedScrollbarPosition();
+   
+   },600)
 
- const renderMessage = ()=>{
-  const inputMessage = inputEl.value;
-  userMessage(inputMessage);
-  inputEl.value = "";
-  
+   
+
 }
 
- sendEl.addEventListener("click",renderMessage);
+
+
+
+function GetAnswer(ans){
+    const res  = ans;
+//     getChat(ans,"chatbot_message");
+ }
+
+
+function renderMessage (){
+   const inputMessage = txtinputEl.value;
+   // console.log(inputMessage);
+    getChat(inputMessage,"user");
+   txtinputEl.value = "";
+    toggleLoading(false);
+   const ans = responseobj[inputMessage];
+   if(ans){
+      GetAnswer(ans)
+      setTimeout(()=>{
+       getChat(ans,"chatbot_message",600)
+       toggleLoading(true);
+      
+      },600)
+   }else{
+      GetAnswer("Please enter right context");
+      setTimeout(()=>{
+         getChat("Please enter correct message")
+         toggleLoading(true);
+      },600);
+   }
+
+    
+}
+
+
+
+sendEl.addEventListener("click",renderMessage);
 
 
